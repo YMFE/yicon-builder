407e354abcdb75945e24de4f0489bec0c2343ede
Joy Du <dooyoe@gmail.com>
2017-04-10 14:21:02 +0800

    update doc

--- _docs/source/app.js
+++ _docs/source/app.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     var $contentLeft = $('.content-left');
     var $contentLeftWidth = $contentLeft.width() - 1;
     var $ydoc = $('.ydoc');
-    var $mask = $('.mask');
     var isPanelHide = true;
     var winWidth = $(window).width();
     var h2 = $('.content-right').find('h2');
@@ -43,14 +42,10 @@ $(document).ready(function() {
     }
     titles.sort(sortAsOffset('offsetTop'));
 
-    $openPanel.on('click',function(){
+    $openPanel.on('tap',function(){
         if(isPanelHide){    // 点击弹出panel
             isPanelHide = false;
             $ydoc.addClass('hidden');
-            $mask.show();
-            setTimeout(function(){
-                $mask.addClass('show');
-            },50)
             $openPanel.css({
                  'transform':'translateX(-'+$contentLeftWidth+'px)'
             })
@@ -60,10 +55,6 @@ $(document).ready(function() {
         }else {     // 点击隐藏panel
             isPanelHide = true;
             $ydoc.removeClass('hidden');
-            $mask.removeClass('show');
-            setTimeout(function(){
-                $mask.hide();
-            },400)
             $openPanel.css({
                  'transform':'translateX(0px)'
             })
@@ -88,6 +79,10 @@ $(document).ready(function() {
             }
         }
     });
+    $openPanel.on('touchstart',function(event){
+        event.preventDefault();
+        $openPanel.trigger('tap');
+    })
 
     $ydoc.removeClass('hidden');
     $ydoc.on('scroll', function(){
@@ -98,11 +93,19 @@ $(document).ready(function() {
         $ydoc.scrollTop(sessionStorage.offsetTop);
     }
     // $openPanel.trigger('click');
-    $mask.on('click',function(){
+    $('.content-right').on('touchstart',function(){
         if(!isPanelHide){
-            $openPanel.click();
+            $openPanel.trigger('tap');
         }
     });
+
+    // 移动端侧栏展开时禁止主页面滚动
+    $('.content-right').on('touchmove',function(e){
+        if(!isPanelHide){
+            e.preventDefault();
+        }
+    });
+
 
     // PC端导航
     $('.navbar-toggle').click(function(){
@@ -143,14 +146,12 @@ $(document).ready(function() {
     var docSideNav = $('.docs-sidenav');
     var  ydocContainerCon= $('.ydoc-container-content');
     if(winWidth>767){
-        docSideNav.width($contentLeftWidth);
+        docSideNav.width(ydocContainerCon.width()*0.25);
     }
     if (sidebar.height() > winHeight) {
         sidebar.css('max-height', winHeight + 'px');
         $('.docs-sidenav').css('max-height', winHeight + 'px');
-        if(winWidth<768){
-            $('.docs-sidenav').css({'overflow-x':'hidden'});
-        }
+        $('.docs-sidenav').css({'overflow-x':'hidden'});
         var activeMenu,
             barScroll = false;
 
@@ -202,7 +203,7 @@ $(document).ready(function() {
         var docSideNav = $('.docs-sidenav');
         var ydocContainerCon= $('.ydoc-container-content');
         if(winWidth>767){
-            docSideNav.width($contentLeftWidth);
+            docSideNav.width(ydocContainerCon.width()*0.25);
         }
         if (sidebar.height() > winHeight) {
             sidebar.css('max-height', winHeight + 'px');
